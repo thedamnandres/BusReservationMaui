688f8ba10f179dfaa118028ef5eabbbdbdb3f5e6
roberto.guaña <robertofelipe20.rf@gmail.com>
2024-11-30 22:58:56 -0500

    COMMIT BOLETOS

--- BusReservationMaui/Models/Boleto.cs
+++ BusReservationMaui/Models/Boleto.cs
@@ -1,6 +1,12 @@
-namespace BusReservationMaui.Models;
+using System;
 
-public class Boleto
+namespace BusReservationMaui.Models
 {
-    
+    public class Boleto
+    {
+        public string PasajeroNombre { get; set; }
+        public string Asiento { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public DateTime FechaViaje { get; set; }
+    }
 }
