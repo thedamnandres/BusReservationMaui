a5fef32b5207cd46314ee7b9704a8c986733f4b5
Andres Jimenez <andres_jimenez666@hotmail.com>
2024-12-03 09:22:13 -0500

    Creacion archivo Reservas View Model

---
+++ BusReservationMaui/ViewModels/ReservasViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using BusReservationMaui.Models;
+
+namespace BusReservationMaui.ViewModels;
+
+public class ReservasViewModel
+{
+   
+}
